f0fe25e5916c1dcb5a703d9fd10b0882616e574f
aakashrajput <aakashsingh1999@gmail.com>
2021-01-09 12:26:03 +0530

    Created Api Login Route

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -4,8 +4,24 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Auth;
 
 class AuthController extends Controller
 {
-    //
+    public function login(Request $request)
+    {
+        $creds = $request->only(['email', 'password']);
+        
+        if(!$token=auth()->attempt($creds)) {
+            return response()->json([
+                'success' => false 
+            ]);
+        }
+
+        return response()->json([
+            'success' => true,
+            'token' => $token,
+            'user' => Auth::user()
+        ]);
+    }
 }
